9bde4f226a8ccafe5c5c6795e548e19e15b19615
Nester Glover <nesterglover@hotmail.com>
2020-06-02 16:42:01 +0700

    should_see_login_link test case

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,14 @@
+from pages.main_page import MainPage
 from selenium import webdriver
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)  # инициализация MainPage
+    page.open()                     # открытие страницы
+    page.go_to_login_page()         # переход на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
